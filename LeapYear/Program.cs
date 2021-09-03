7b64b9a7d1cd2fe62de6565e75ece82438f213bb
Andreas Nicolaj Tietgen <andreotietgen@hotmail.com>
2021-09-03 21:51:52 +0200

    Implement user interaction

--- LeapYear/Program.cs
+++ LeapYear/Program.cs
@@ -6,7 +6,18 @@ namespace LeapYear
     {
         public static void Main(string[] args)
         {
-            IsLeapYear(2000);
+            Start:
+            Console.Write("Type in a year: ");
+            string inputYear = Console.ReadLine();
+            int year;
+
+            if(!int.TryParse(inputYear, out year))
+                goto Start;    
+            
+            if(IsLeapYear(year))
+                System.Console.WriteLine("Yay");
+            else
+                System.Console.WriteLine("Nay");
         }
 
         public static bool IsLeapYear(int year) 
